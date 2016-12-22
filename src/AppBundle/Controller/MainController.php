470cc1e346ae4e813a13f6dd00b0c9de26f1f9bb
ngdo-pro <ngdo27@gmail.com>
2016-12-22 13:53:06 +0100

    added phpstorm default comments

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -4,8 +4,15 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/**
+ * Class MainController
+ * @package AppBundle\Controller
+ */
 class MainController extends Controller
 {
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction()
     {
         return $this->render('app/main/index.html.twig');
