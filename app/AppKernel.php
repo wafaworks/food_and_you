18b4a1c136a8452bc84f9d4873390f18684766c3
ngdo-pro <ngdo27@gmail.com>
2016-12-22 15:21:00 +0100

    installed FOSUserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new UserBundle\UserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
---
+++ src/UserBundle/Entity/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace UserBundle\Entity;
+
+
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * User
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
